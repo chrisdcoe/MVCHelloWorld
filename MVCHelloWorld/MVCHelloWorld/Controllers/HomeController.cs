64fc25a48cf7540885b9c8d54c019b64bdec48fb
Christopher Coe <christopherdcoe@gmail.com>
2017-05-15 19:14:30 -0400

    Index page calls for a form to be made now. Program reads first and last name and greets user with input, capitalized.

--- MVCHelloWorld/MVCHelloWorld/Controllers/HomeController.cs
+++ MVCHelloWorld/MVCHelloWorld/Controllers/HomeController.cs
@@ -11,12 +11,18 @@ namespace MVCHelloWorld.Controllers
     {
         public ActionResult Index()
         {
-            Person person = new Person { FirstName = "Firsty", LastName = "Lastman" };
+            Person person = new Person { FirstName = "", LastName = "" };
 
 
             return View(person);
         }
 
+        public ActionResult SayHello(Person person)
+        {
+            person.FirstName = person.FirstName.ToUpper();
+            person.LastName = person.LastName.ToUpper();
 
+            return View(person);
+        }
     }
 }
